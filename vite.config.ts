da8a75ab3bbb4467ea0e20e0915f6d46a52b5ac1
Bibhuti05 <bibhuticodes@gmail.com>
2026-06-21 18:09:20 +0530

    feat: remove workflow, integrate EmailJS + react-hot-toast for contact form

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.DEPLOY_TARGET === 'gh-pages' ? '/1IN1B/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
